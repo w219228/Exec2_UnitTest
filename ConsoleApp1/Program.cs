f523d7af103c24cdf101dd9b334af4db63836d8c
w2192 <w2192@LAPTOP-2BO0FNQF>
2022-10-27 23:55:40 +0800

    m

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Ispan.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var obj = new TaiwainStockUtility();
+			DateTime dateTime = new DateTime(2022,10,23,12,22,22);
+			bool canTrade = obj.IsTradingTime(dateTime);
+			Console.WriteLine(canTrade);
+		}
+	}
+}
---
+++ Ispan.Utilities/TaiwainStockUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispan.Utilities
+{
+	public class TaiwainStockUtility
+	{
+		public virtual bool IsTradingTime(DateTime dt)
+		{
+			bool ordinary = isOrdinary(dt);
+			bool trade = isTrade(dt);
+			return (ordinary && trade)?true:false;
+
+			
+		}
+		public virtual bool isOrdinary(DateTime dt)
+		{
+			DayOfWeek week = dt.DayOfWeek;
+			int value = (int)week;
+			return (value == 6 || value == 0) ? false : true;
+		}
+		public virtual bool isTrade(DateTime dt)
+		{
+			int todayTime = dt.Hour;
+			return (todayTime < 9 || todayTime > 13) ? false : true;
+		}
+	}
+}
